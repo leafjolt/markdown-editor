202e58a6c08dae8c67f8d0ad366ef39ab5222e42
Shaan Desai <leafjolt@gmail.com>
2026-02-16 10:57:15 +0100

    Incorporated Vue functionality

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-const input = document.getElementById("input");
-const output = document.getElementById("output");
-input.value = localStorage.getItem("text");
-output.innerHTML = marked.parse(input.value);
-input.oninput = () => {
-    localStorage.setItem("text", input.value);
-    output.innerHTML = marked.parse(input.value);
-}
+// const input = document.getElementById("input");
+// const output = document.getElementById("output");
+// input.value = localStorage.getItem("text");
+// output.innerHTML = marked.parse(input.value);
+// input.oninput = () => {
+//     localStorage.setItem("text", input.value);
+//     output.innerHTML = marked.parse(input.value);
+// }
+
+const app = Vue.createApp({
+    data() {
+        return {
+            message: ''
+        };
+    },
+    computed: {
+        htmlOutput() {
+            return marked.parse(this.message);
+        },
+        charCount() {
+            return this.htmlOutput.replace(/<[^>]*>/g, '').length;
+        }
+    }
+});
+
+app.mount('#app');
