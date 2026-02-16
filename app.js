184be393c483e2e4c6866b611afeea5558ff1b04
Shaan Desai <leafjolt@gmail.com>
2026-02-16 10:36:31 +0100

    Fixed a lot of styling and added initial Markdown functionality

---
+++ app.js
@@ -0,0 +1,8 @@
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+input.value = localStorage.getItem("text");
+output.innerHTML = marked.parse(input.value);
+input.oninput = () => {
+    localStorage.setItem("text", input.value);
+    output.innerHTML = marked.parse(input.value);
+}
